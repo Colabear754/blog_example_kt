939c2d1bd3fc8646f1ebc8658d976365f10b7aa7
Colabear754 <jky2406@gmail.com>
2022-12-22 07:57:57 +0900

    Initial commit

---
+++ src/main/kotlin/com/colabear754/blog_example_kt/BlogExampleKtApplication.kt
@@ -0,0 +1,11 @@
+package com.colabear754.blog_example_kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogExampleKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlogExampleKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/colabear754/blog_example_kt/BlogExampleKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.colabear754.blog_example_kt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogExampleKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
