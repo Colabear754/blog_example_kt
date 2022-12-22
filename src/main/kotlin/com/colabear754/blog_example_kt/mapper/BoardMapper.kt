c212f7ce9e52eeba602ac7d59e65725581f6a3b2
Colabear754 <jky2406@gmail.com>
2022-12-22 11:45:59 +0900

    블로그 글 Mapper

--- src/main/kotlin/com/colabear754/blog_example_kt/mapper/BoardMapper.kt
+++ src/main/kotlin/com/colabear754/blog_example_kt/mapper/BoardMapper.kt
@@ -1,7 +1,9 @@
 package com.colabear754.blog_example_kt.mapper
 
 import com.colabear754.blog_example_kt.domain.BoardDTO
+import org.apache.ibatis.annotations.Mapper
 
+@Mapper
 interface BoardMapper {
     fun getDocuments(params: Map<String, Any?>): List<BoardDTO>
     fun getDocument(seq: Int): BoardDTO
