e7f3ba2054ad218055af8c767053f1dec1b55071
Colabear754 <jky2406@gmail.com>
2022-12-22 11:45:43 +0900

    블로그 글 Mapper

---
+++ src/main/kotlin/com/colabear754/blog_example_kt/mapper/BoardMapper.kt
@@ -0,0 +1,15 @@
+package com.colabear754.blog_example_kt.mapper
+
+import com.colabear754.blog_example_kt.domain.BoardDTO
+
+interface BoardMapper {
+    fun getDocuments(params: Map<String, Any?>): List<BoardDTO>
+    fun getDocument(seq: Int): BoardDTO
+    fun getDocumentsCount(category_id: Int): Int
+    fun write(document: BoardDTO)
+    fun update(document: BoardDTO): Int
+    fun delete(document: BoardDTO): Int
+    fun isLike(params: Map<String, Any?>): Boolean
+    fun like(params: Map<String, Any?>): Int
+    fun cancelLike(params: Map<String, Any?>): Int
+}
