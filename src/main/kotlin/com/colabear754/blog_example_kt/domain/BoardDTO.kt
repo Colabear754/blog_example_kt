5e7c6e0ad0f52e37fb332a637c330badeef8f030
Colabear754 <jky2406@gmail.com>
2022-12-22 15:48:42 +0900

    날짜 출력 포맷 변경

--- src/main/kotlin/com/colabear754/blog_example_kt/domain/BoardDTO.kt
+++ src/main/kotlin/com/colabear754/blog_example_kt/domain/BoardDTO.kt
@@ -1,14 +1,16 @@
 package com.colabear754.blog_example_kt.domain
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class BoardDTO(
     var seq: Int,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     var reg_time: LocalDateTime,
-    var category_id: Int,
+    var category_id: Int?,
     var subject: String,
     var content: String,
-    var thumbnail: String,
+    var thumbnail: String?,
     var view_cnt: Int,
     var like_cnt: Int
 )
